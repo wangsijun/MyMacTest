250112fffbd49fcf3880c112b54207e105bdf349
wangsijun <wangsijun1986@vip.qq.com>
2016-02-26 00:19:25 +0800

    Initial Commit

---
+++ Unified/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Unified
+//
+//  Created by 王思俊 on 16/2/26.
+//  Copyright © 2016年 王思俊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
